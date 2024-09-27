86b88d5c7055844e12eba98084875ff8d6043592
adeenazara2002 <adinazara2001@gmail.com>
2024-09-28 02:39:18 +0500

    Onboard Screen

--- lib/Screens/onBoard1.dart
+++ lib/Screens/onBoard1.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class OnBoard extends StatefulWidget {
   const OnBoard({super.key});
@@ -27,17 +26,17 @@ class _OnBoardState extends State<OnBoard> {
                 Image.asset('assets/images/yellowBall.png'),
               ],
             ),
-            SizedBox(
-              height: 10,
-            ),
+            // SizedBox(
+            //   height: 1,
+            // ),
             Row(
               children: [
                 Padding(padding: EdgeInsets.only(left: 30)),
                 Image.asset('assets/images/pinkMiddle.png'),
-                Padding(padding: EdgeInsets.only(left: 50)),
+                Padding(padding: EdgeInsets.only(left: 90)),
                 Container(
                   height: 60,
-                  width: 200,
+                  width: 180,
                   decoration: BoxDecoration(
                     gradient: LinearGradient(colors: [
                       Color.fromRGBO(153, 153, 153, 0.25),
@@ -49,20 +48,21 @@ class _OnBoardState extends State<OnBoard> {
                   ),
                   child: Column(
                     children: [
-                      SizedBox(height: 20,),
+                      SizedBox(
+                        height: 20,
+                      ),
                       Row(
-          
                         children: [
                           Padding(padding: EdgeInsets.only(left: 90)),
                           Image.asset('assets/images/bigLine.png'),
                         ],
                       ),
-                      SizedBox(height: 10,),
-                    
+                      SizedBox(
+                        height: 10,
+                      ),
                       Row(
                         children: [
                           Padding(padding: EdgeInsets.only(left: 130)),
-
                           Image.asset('assets/images/smallLine.png'),
                         ],
                       ),
@@ -77,17 +77,76 @@ class _OnBoardState extends State<OnBoard> {
                 Image.asset('assets/images/pinkBall.png'),
               ],
             ),
-            
             Row(
               children: [
-                Padding(padding: EdgeInsets.only(left: 260)),
-
-                CircleAvatar(backgroundImage: AssetImage('assets/images/girl.png'),
-                radius: 30,),
+                Padding(padding: EdgeInsets.only(left: 270)),
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/images/girl.png'),
+                  radius: 30,
+                ),
               ],
             ),
+
+            SizedBox(height: 70,),
             SizedBox(
-              height: 170,
+              height: 120, // Height of the stack
+              child: Stack(
+                clipBehavior: Clip.none, // Allow overflow for overlap
+                children: [
+                  // CircleAvatar (red head)
+                  Positioned(
+                    top: 0, // Place red head at the base
+                    left: 20, // Adjust positioning of avatar
+                    child: CircleAvatar(
+                      backgroundImage: AssetImage('assets/images/redHead.png'),
+                      radius: 40, // Adjust radius
+                    ),
+                  ),
+                  // Grey Container overlapping the top right of redhead image
+                  Positioned(
+                    top: -20, // Adjust overlap to make container appear higher
+                    left:
+                        70, // Adjust positioning to overlap the redhead image
+                    child: Container(
+                      height: 60,
+                      width: 180,
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [
+                            Color.fromRGBO(153, 153, 153, 0.25),
+                            Color.fromRGBO(202, 202, 202, 0.25),
+                          ],
+                        ),
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(
+                          color: Color.fromRGBO(202, 202, 202, 0.25),
+                        ),
+                      ),
+                      child: Column(
+                        children: [
+                          SizedBox(height: 20),
+                          Row(
+                            children: [
+                              Padding(padding: EdgeInsets.only(left: 90)),
+                              Image.asset('assets/images/bigLine.png'),
+                            ],
+                          ),
+                          SizedBox(height: 10),
+                          Row(
+                            children: [
+                              Padding(padding: EdgeInsets.only(left: 130)),
+                              Image.asset('assets/images/smallLine.png'),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 10,
             ),
             Row(
               children: [
@@ -104,11 +163,8 @@ class _OnBoardState extends State<OnBoard> {
                 Image.asset('assets/images/purpleBall.png'),
               ],
             ),
-
-            // SizedBox(height: 10,),
             Row(
               children: [
-                // Padding(padding: EdgeInsets.only(left: 260)),
                 Image.asset('assets/images/yellowBigBall.png'),
               ],
             ),

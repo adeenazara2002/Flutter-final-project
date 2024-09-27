52ed3348a5a9c59214d00c1b06bb7d0d1bf4dd46
adeenazara2002 <adinazara2001@gmail.com>
2024-09-28 01:05:26 +0500

    Onboard Screen

--- lib/Screens/onBoard1.dart
+++ lib/Screens/onBoard1.dart
@@ -23,7 +23,65 @@ class _OnBoardState extends State<OnBoard> {
                 Padding(padding: EdgeInsets.only(left: 300)),
                 Image.asset('assets/images/yellowBall.png'),
               ],
-            )
+            ),
+
+            SizedBox(height: 10,),
+            Row(
+              children: [
+                Padding(padding: EdgeInsets.only(left: 30)),
+                Image.asset('assets/images/pinkMiddle.png'),
+              ],
+            ),
+
+            SizedBox(height: 10,),
+            Row(
+              children: [
+                Padding(padding: EdgeInsets.only(left: 320)),
+                Image.asset('assets/images/pinkBall.png'),
+              ],
+            ),
+            
+            SizedBox(height: 170,),
+            Row(
+              children: [
+                Padding(padding: EdgeInsets.only(left: 260)),
+                Image.asset('assets/images/purpleBall.png'),
+              ],
+            ),
+
+
+            SizedBox(height: 100,),
+            Row(
+              children: [
+                Padding(padding: EdgeInsets.only(left: 80)),
+                Image.asset('assets/images/purpleBall.png'),
+              ],
+            ),
+
+            SizedBox(height: 70,),
+            Row(
+              children: [
+                Padding(padding: EdgeInsets.only(left: 260)),
+                Image.asset('assets/images/yellowBall.png'),
+              ],
+            ),
+
+            SizedBox(height: 10,),
+            Row(
+              children: [
+                Padding(padding: EdgeInsets.only(left: 300)),
+                Image.asset('assets/images/purpleBall.png'),
+              ],
+            ),
+
+            SizedBox(height: 50,),
+            Row(
+              children: [
+                Padding(padding: EdgeInsets.only(left: 250)),
+                Image.asset('assets/images/pinkBall.png'),
+              ],
+            ),
+
           ],
         ),
       ),

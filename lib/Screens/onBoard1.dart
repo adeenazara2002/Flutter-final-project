49cdbae86dd130214f430839d24699035d9e6198
adeenazara2002 <adinazara2001@gmail.com>
2024-09-28 01:46:00 +0500

    Onboard Screen

--- lib/Screens/onBoard1.dart
+++ lib/Screens/onBoard1.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class OnBoard extends StatefulWidget {
   const OnBoard({super.key});
@@ -17,71 +18,127 @@ class _OnBoardState extends State<OnBoard> {
         color: Colors.white,
         child: Column(
           children: [
-            SizedBox(height: 80,),
+            SizedBox(
+              height: 80,
+            ),
             Row(
               children: [
                 Padding(padding: EdgeInsets.only(left: 300)),
                 Image.asset('assets/images/yellowBall.png'),
               ],
             ),
-
-            SizedBox(height: 10,),
+            SizedBox(
+              height: 10,
+            ),
             Row(
               children: [
                 Padding(padding: EdgeInsets.only(left: 30)),
                 Image.asset('assets/images/pinkMiddle.png'),
+                Padding(padding: EdgeInsets.only(left: 50)),
+                Container(
+                  height: 60,
+                  width: 200,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(colors: [
+                      Color.fromRGBO(153, 153, 153, 0.25),
+                      Color.fromRGBO(202, 202, 202, 0.25),
+                    ]),
+                    borderRadius: BorderRadius.circular(12),
+                    border:
+                        Border.all(color: Color.fromRGBO(202, 202, 202, 0.25)),
+                  ),
+                  child: Column(
+                    children: [
+                      SizedBox(height: 20,),
+                      Row(
+          
+                        children: [
+                          Padding(padding: EdgeInsets.only(left: 90)),
+                          Image.asset('assets/images/bigLine.png'),
+                        ],
+                      ),
+                      SizedBox(height: 10,),
+                    
+                      Row(
+                        children: [
+                          Padding(padding: EdgeInsets.only(left: 130)),
+
+                          Image.asset('assets/images/smallLine.png'),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
-
-            SizedBox(height: 10,),
             Row(
               children: [
-                Padding(padding: EdgeInsets.only(left: 320)),
+                Padding(padding: EdgeInsets.only(left: 330)),
                 Image.asset('assets/images/pinkBall.png'),
               ],
             ),
             
-            SizedBox(height: 170,),
+            Row(
+              children: [
+                Padding(padding: EdgeInsets.only(left: 260)),
+
+                CircleAvatar(backgroundImage: AssetImage('assets/images/girl.png'),
+                radius: 30,),
+              ],
+            ),
+            SizedBox(
+              height: 170,
+            ),
             Row(
               children: [
                 Padding(padding: EdgeInsets.only(left: 260)),
                 Image.asset('assets/images/purpleBall.png'),
               ],
             ),
-
-
-            SizedBox(height: 100,),
+            SizedBox(
+              height: 100,
+            ),
             Row(
               children: [
-                Padding(padding: EdgeInsets.only(left: 80)),
+                Padding(padding: EdgeInsets.only(left: 100)),
                 Image.asset('assets/images/purpleBall.png'),
               ],
             ),
 
-            SizedBox(height: 70,),
+            // SizedBox(height: 10,),
+            Row(
+              children: [
+                // Padding(padding: EdgeInsets.only(left: 260)),
+                Image.asset('assets/images/yellowBigBall.png'),
+              ],
+            ),
+            SizedBox(
+              height: 70,
+            ),
             Row(
               children: [
                 Padding(padding: EdgeInsets.only(left: 260)),
                 Image.asset('assets/images/yellowBall.png'),
               ],
             ),
-
-            SizedBox(height: 10,),
+            SizedBox(
+              height: 10,
+            ),
             Row(
               children: [
                 Padding(padding: EdgeInsets.only(left: 300)),
                 Image.asset('assets/images/purpleBall.png'),
               ],
             ),
-
-            SizedBox(height: 50,),
+            SizedBox(
+              height: 50,
+            ),
             Row(
               children: [
                 Padding(padding: EdgeInsets.only(left: 250)),
                 Image.asset('assets/images/pinkBall.png'),
               ],
             ),
-
           ],
         ),
       ),

fdd766c2974df10ba29045a1389e0c42800506bf
adeenazara2002 <adinazara2001@gmail.com>
2024-09-28 02:57:28 +0500

    Onboard Screen

--- lib/Screens/onBoard1.dart
+++ lib/Screens/onBoard1.dart
@@ -18,7 +18,7 @@ class _OnBoardState extends State<OnBoard> {
         child: Column(
           children: [
             SizedBox(
-              height: 80,
+              height: 50,
             ),
             Row(
               children: [
@@ -87,12 +87,20 @@ class _OnBoardState extends State<OnBoard> {
               ],
             ),
 
-            SizedBox(height: 70,),
+            SizedBox(
+              height: 20,
+            ),
             SizedBox(
               height: 120, // Height of the stack
               child: Stack(
                 clipBehavior: Clip.none, // Allow overflow for overlap
                 children: [
+                  Row(
+              children: [
+                Padding(padding: EdgeInsets.only(left: 260)),
+                Image.asset('assets/images/purpleBall.png'),
+              ],
+            ),
                   // CircleAvatar (red head)
                   Positioned(
                     top: 0, // Place red head at the base
@@ -105,8 +113,7 @@ class _OnBoardState extends State<OnBoard> {
                   // Grey Container overlapping the top right of redhead image
                   Positioned(
                     top: -20, // Adjust overlap to make container appear higher
-                    left:
-                        70, // Adjust positioning to overlap the redhead image
+                    left: 70, // Adjust positioning to overlap the redhead image
                     child: Container(
                       height: 60,
                       width: 180,
@@ -145,21 +152,70 @@ class _OnBoardState extends State<OnBoard> {
                 ],
               ),
             ),
+
             SizedBox(
-              height: 10,
+              height: 50, // Height of the stack
+              child: Stack(
+                clipBehavior: Clip.none, // Allow overflow for overlap
+                children: [
+                  // CircleAvatar (red head)
+                  Positioned(
+                    top: 0, // Place red head at the base
+                    left: 260, // Adjust positioning of avatar
+                    child: CircleAvatar(
+                      backgroundImage: AssetImage('assets/images/uncle.png'),
+                      radius: 40, // Adjust radius
+                    ),
+                  ),
+                  // Grey Container overlapping the top right of redhead image
+                  Positioned(
+                    top: -20, // Adjust overlap to make container appear higher
+                    left: 100, // Adjust positioning to overlap the redhead image
+                    child: Container(
+                      height: 60,
+                      width: 180,
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [
+                            Color.fromRGBO(153, 153, 153, 0.25),
+                            Color.fromRGBO(202, 202, 202, 0.25),
+                          ],
+                        ),
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(
+                          color: Color.fromRGBO(202, 202, 202, 0.25),
+                        ),
+                      ),
+                      child: Column(
+                        children: [
+                          SizedBox(height: 20),
+                          Row(
+                            children: [
+                              Padding(padding: EdgeInsets.only(left: 90)),
+                              Image.asset('assets/images/bigLine.png'),
+                            ],
+                          ),
+                          SizedBox(height: 10),
+                          Row(
+                            children: [
+                              Padding(padding: EdgeInsets.only(left: 130)),
+                              Image.asset('assets/images/smallLine.png'),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            
+            SizedBox(
+              height: 5,
             ),
             Row(
               children: [
-                Padding(padding: EdgeInsets.only(left: 260)),
-                Image.asset('assets/images/purpleBall.png'),
-              ],
-            ),
-            SizedBox(
-              height: 100,
-            ),
-            Row(
-              children: [
-                Padding(padding: EdgeInsets.only(left: 100)),
+                Padding(padding: EdgeInsets.only(left: 80)),
                 Image.asset('assets/images/purpleBall.png'),
               ],
             ),
@@ -168,27 +224,27 @@ class _OnBoardState extends State<OnBoard> {
                 Image.asset('assets/images/yellowBigBall.png'),
               ],
             ),
-            SizedBox(
-              height: 70,
-            ),
+            // SizedBox(
+            //   height: 20,
+            // ),
             Row(
               children: [
                 Padding(padding: EdgeInsets.only(left: 260)),
                 Image.asset('assets/images/yellowBall.png'),
               ],
             ),
-            SizedBox(
-              height: 10,
-            ),
+            // SizedBox(
+            //   height: 10,
+            // ),
             Row(
               children: [
                 Padding(padding: EdgeInsets.only(left: 300)),
                 Image.asset('assets/images/purpleBall.png'),
               ],
             ),
-            SizedBox(
-              height: 50,
-            ),
+            // SizedBox(
+            //   height: 10,
+            // ),
             Row(
               children: [
                 Padding(padding: EdgeInsets.only(left: 250)),

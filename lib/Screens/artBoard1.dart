b019c889a38260cdedabbde79d5db525b5b88c97
adeenazara2002 <adinazara2001@gmail.com>
2024-09-27 19:09:11 +0500

    Art Board Screen

---
+++ lib/Screens/artBoard1.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ArtBoard extends StatefulWidget {
+  const ArtBoard({super.key});
+
+  @override
+  State<ArtBoard> createState() => _ArtBoardState();
+}
+
+class _ArtBoardState extends State<ArtBoard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
